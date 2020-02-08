d05fa6d0f3aab4744c04eab138ece45bce12f687
Santiago Pinilla <johansantiagopinillauribe@gmail.com>
2020-02-07 20:15:40 -0500

    Inicio del curso de kotlin

---
+++ src/HelloWorld.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>) {
+    println("Hola Mundo, por devcamilo, :3")
+
+    val a = 10
+    val b = 5
+
+    println(a.plus(b))
+    println(a.minus(b))
+    println(a.times(b))
+    println(a.div(b))
+
+    val name = "Santiago"
+    if (name == "Santiago") {
+        println(name)
+    }
+    else {
+        println("Error, nombre incorrecto")
+    }
+
+}
